e4540829288acdd6ff2d79db9d5b5733fbe09c0b
Sibo Zhu <siboz1995@gmail.com>
2020-07-15 01:04:44 -0400

    small

--- collect_rgb_depth_lidar.py
+++ collect_rgb_depth_lidar.py
@@ -267,7 +267,7 @@ def main():
         actor_list.append(rgb_front)
         actor_list.append(my_car)
 
-        # Camera Instrinsic Matric
+        # Camera Instrinsic Transformation
         instrinsic_filename = SAVE_PATH+NAME_WITH_TIME+'/calib/calib_cam_to_cam.txt'
         extrinsic_filename = SAVE_PATH+NAME_WITH_TIME+'/calib/calib_velo_to_cam.txt'
 
@@ -282,21 +282,20 @@ def main():
         P0 = np.ravel(P0, order='C')
         R0 = np.identity(3)
 
-        # LiDAR to Camera transformation
+        # LiDAR to Camera Transformation
         Rotation = np.array([[0, -1, 0],
                                 [0, 0, -1],
                                 [1, 0, 0]])
         # Add translation vector from velo to camera. This is 0 because the position of camera and lidar is equal in our configuration.
-        # Translation = np.column_stack((TR_velodyne, np.array([0, 0, 0])))
         Translation = np.array([0, 0, 0])
 
         with open(extrinsic_filename, 'w') as f:
-            f.write("calib_time:" + NAME_WITH_TIME + "\n")
+            f.write("calib_time: " + NAME_WITH_TIME + "\n")
             write_flat(f, "R", Rotation)
             write_flat(f, "T", Translation)
 
         with open(instrinsic_filename, 'w') as f:
-            f.write("calib_time:" + NAME_WITH_TIME + "\n")
+            f.write("calib_time: " + NAME_WITH_TIME + "\n")
             write_flat(f, "P_rect_02" , P0)
             write_flat(f, "R_rect_02", R0)
 
